95914299d20ff5dcb42ea0486d96949d34c51c2c
Aleksandr Abramov <aleksandr.a@findbride.com>
2025-07-19 15:34:07 +0700

    003

---
+++ src/Cli.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace BrainGames\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function greetWelcomeMessage()
+{
+    line('Welcome to the Brain Game!');
+}
+
+function askName()
+{
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
+
+
+
